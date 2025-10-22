35fc0afdcde00fa7f9467c6134d705059e7a5395
Taiwo Owoseje <taiwoowoseje@gmail.com>
2025-10-22 15:28:07 +0100

    work in progress

--- Day_2/GreetingCard/src/App.jsx
+++ Day_2/GreetingCard/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import GreetingCard from './GreetingCard';
+import GreetingCard from './components/GreetingCard';
 
 function App() {
 
--- Day_2/GreetingCard/src/components/GreetingCard.jsx
+++ Day_2/GreetingCard/src/components/GreetingCard.jsx
@@ -1,7 +1,7 @@
 function GreetingCard({ name, message, color}) {
     return (
         <div className="container" style={{ background: color}}>
-            <h1 className="greeting">My name is {name}</h1>
+            <h1 className="greeting">I am {name}</h1>
             <p className="message">{message}</p>
         </div>
     )
