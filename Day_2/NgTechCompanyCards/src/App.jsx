a7abf285f99b1611b3c502464c72284087fdde29
Taiwo Owoseje <taiwoowoseje@gmail.com>
2025-11-06 15:27:12 +0100

    work in progress

---
+++ Day_2/NgTechCompanyCards/src/App.jsx
@@ -0,0 +1,37 @@
+import CompanyCard from './CompanyCard';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Top Nigerian Tech Companies</h1>
+      <div className="company-grid">
+        <CompanyCard 
+          name="Paystack"
+          industry="Fintech"
+          location="Lagos"
+          employees="200+"
+          logo="paystack-logo.png"
+          featured={true}
+        />
+        <CompanyCard 
+          name="Flutterwave"
+          industry="Payment Solutions"
+          location="Lagos"
+          employees="500+"
+          logo="flutterwave-logo.png"
+          featured={true}
+        />
+        <CompanyCard 
+          name="Andela"
+          industry="Tech Talent"
+          location="Lagos"
+          employees="1000+"
+          logo="andela-logo.png"
+          featured={false}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
